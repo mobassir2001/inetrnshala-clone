176b6c6093cac56c5c1b084a5a466281e0ca4110
mobassir2001 <mobassir2001@gmail.com>
2023-08-28 13:45:41 +0530

    second part complete

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT
 const logger = require("morgan");
 app.use(logger("tiny"))
 
+//routes
 app.use("/",require("./routes/indexRoutes"))
+
+//error handling
+const ErrorHandler = require("./utils/ErrorHandler");
+const {generatederrors} = require("./middlewares/error")
+
+app.all("*",(req,res,next)=>{
+    next(new ErrorHandler(`Requested url not found ${req.url}`,404))
+})
+
+app.use(generatederrors)
 
 app.listen(PORT,()=>{ console.log(`server running on port ${PORT}`)});
