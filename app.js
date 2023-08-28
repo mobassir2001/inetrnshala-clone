ac9db62d4e3c323b78b1e2bdccba3be6fdd76980
mobassir2001 <mobassir2001@gmail.com>
2023-08-28 12:37:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config({path:"./.env"})
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT
+  
+// LOGGER
+const logger = require("morgan");
+app.use(logger("tiny"))
+
+app.use("/",require("./routes/indexRoutes"))
+
+app.listen(PORT,()=>{ console.log(`server running on port ${PORT}`)});
